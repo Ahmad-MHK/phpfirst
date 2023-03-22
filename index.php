924762c3bc6e910e64719cc18bed075b8f2ed477
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2023-03-22 12:13:56 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="layout.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-line">
+                <li class="row-nav">
+                    <a href="index.html"><div class="naam-nav"><h3>Monkey Restaurant</h3></div></a>
+                    <a href="index.html"><div class="Menu-nav">Menu</div></a>
+                    <a href=""><div class="Orders-nav">Orders</div></a>
+                    <a href=""><div class="contact-nav">Contact</div></a>
+                    <a href="login.html"><div class="login">Login / register</div></a>
+                </li>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <div id="Menu"> 
+            <div class="search-menu-naam">
+                <div class="proc60">
+                    <h1>Menu</h1>
+                </div>
+                <div class="proc40">
+                    <input class="icon"  type="text" value placeholder="Search">
+                </div>
+            </div>
+            <div class="col-menu">
+                <div class="eaten-block-1">
+                    <div class="food-naam">Text naam</div>
+                    <div class="gappe"></div>
+                    <div class="food-omschrijfing">tom en jhon zijn laat</div>
+                    <div class="gappe"></div>
+                    <div class="food-prices"> $4.99</div>
+                </div>
+                <div class="eaten-block-2"></div>
+                <div class="eaten-block-3"></div>
+                <div class="eaten-block-4"></div>
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
